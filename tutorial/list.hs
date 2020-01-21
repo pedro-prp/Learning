89de8318246e9543e6ab9b2a99233632378e6b48
Pedro Rodrigues <peddroprp@gmail.com>
2020-01-21 09:03:51 -0300

    Add basic functions structure definitions

---
+++ tutorial/list.hs
@@ -0,0 +1,10 @@
+-- simple list
+numbers = [4,8,15,16,23,42]
+
+-- concact two lists
+sum_list = [1,2,3] ++ [4,5,6]
+
+-- strings are a char list
+woot = ['w','o'] ++ ['o', 't']
+
+hello = "Hello" ++ " " ++ "World"
