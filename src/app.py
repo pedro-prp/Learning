422624d396150f3a11e15aff5adf68cba8febeb4
Pedro Rodrigues <peddroprp@gmail.com>
2020-09-21 18:38:38 -0300

    parse code string to list of functions and args

--- src/app.py
+++ src/app.py
@@ -5,14 +5,32 @@ def lex(code_str):
 def to_num(token):
     try:
         return float(token)
-    except:
+    except ValueError:
         return token
 
 
-if __name__ == '__main__':
-    code = '(+ 2 1)'
-    
-    code = lex(code)
+def read(tokens):
+    token = tokens.pop(0)
+
+    if token != '(':
+        return token
+
+    res = []
+    while tokens[0] != ')':
+        elem = read(tokens)
+        res.append(elem)
+
+    del tokens[0]
+    return res
+
+
+def parse(code):
     code = [to_num(x) for x in code]
 
-    print(code)
+    return read(code)
+
+
+if __name__ == '__main__':
+    code = '(+ 2 (* 2 1))'
+
+    print(parse(lex(code)))
